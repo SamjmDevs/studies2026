79ba55b53ebb7c49824d7275668473fd7d033e7a
SamjmDevs <samueljmdevs@proton.me>
2026-04-22 21:45:56 +0200

    Añadida carpeta para JS y más apuntes PHP

--- PHP/apuntes.php
+++ PHP/apuntes.php
@@ -5,6 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Prácticas PHP</title>
+    <style>
+        body {
+            background-color: lightgray;
+        }
+    </style>
 </head>
 
 <body>
@@ -13,6 +18,14 @@
     //Comentario de linea
     /*Comentario de bloque*/
     #Comentario de Unix
+    $name = "Pache";
+    ?>
+    </br>
+    <?= "Hola $name" ?>
+    </br>
+    <?= var_dump($name) ?>
+    <?php
+    #var_dump suelta toda la info de la variable
     ?>
 </body>
 
