ef4968b1dedee859065bfc895f090216823e9d90
SamjmDevs <samueljmdevs@proton.me>
2026-04-25 23:45:59 +0200

    Seguimos con operadores

--- Python/02_operadores.py
+++ Python/02_operadores.py
@@ -1,1 +1,23 @@
 #Comenzamos con operaciones
+
+first_number = 7.0
+second_number = 5.0
+
+#Los básicos de toda la vida
+
+print(first_number+second_number)
+print(first_number-second_number)
+print(first_number*second_number)
+print(first_number/second_number)
+
+#Módulo, te da el resto
+
+print(first_number%second_number)
+
+#División floor, se redondea a un número entero
+
+print(first_number//second_number)
+
+#Calculamos exponente
+
+print(first_number**second_number) #Sería 7^5
