e208ba0ff154b8524dfac9284f104417ce06b9dc
SamjmDevs <samueljmdevs@proton.me>
2026-04-26 21:52:34 +0200

    Añadidos apuntes js hasta condicionales

--- JS/script.js
+++ JS/script.js
@@ -17,7 +17,7 @@ let name = "Samu"
 //const => son constantes
 const ALIAS = "Pache"
 
-console.log(hola + " soy " + name + " y me llaman "+ ALIAS)
+console.log(hola + " soy " + name + " y me llaman " + ALIAS)
 
 //Tipos de datos
 //Numéricos
@@ -38,4 +38,76 @@ let nulo = null //Intencionadamente es nula, la necesitamos vacía por algún mo
 let mySymbol = Symbol("mySymbol")
 
 //Mostramos los tipos de datos
-console.log(typeof(granNumb))
+console.log(typeof (granNumb))
+
+//Operadores
+//Aritméticos
+/*
++ => suma
+- => resta
+* => multiplicación
+/ => división
+% => módulo
+** => exponente
+// => división redondeada a la baja
+++ => incremento
+-- => decremento
+*/
+//De asignación
+/**
++=x => suma a la variable x
+-=x => resta a la variable x
+/=x => variable dividida por x
+*=x => variable multiplicada por x
+%=x => el módulo de la variable dividida por x
+**=x => la variable elevada a x 
+*/
+//Comparativos
+/*
+< => menor que
+<= => menor o igual que
+> => mayor que
+>= => mayor o igual que
+== => igual a (por valor)
+=== => igual por identidad (tipo y valor)
+!= => distinto a
+!== => desigualdad por identidad (tipo y valor)
+*/
+//Lógicos
+/*
+&& => y
+|| => o
+! => niega el valor (invierte)
+*/
+
+//Strings
+let miNombre = "Drachenwaschbär"
+let saludo = "Hola, " + miNombre + "!"
+
+console.log(saludo)
+
+//Longitud de string
+console.log(saludo.length)
+//accedemos a caracter
+console.log(saludo[5]) //accedemos a la posición 5, empezamos desde el 0
+
+//Métodos comunes
+console.log(miNombre.toUpperCase())
+console.log(miNombre.toLowerCase())
+console.log(miNombre.indexOf("s"))
+console.log(miNombre.includes("v"))
+console.log(miNombre.slice(0,7))
+console.log(miNombre.replace("Drachen","Drako"))
+
+//Template literals
+let message =`Hola, este es 
+              mi curso de javascript`
+
+console.log(message)
+
+//Interpolación de variables
+let email ="drachen@wsachbar.com"
+let messageIterpolado = `Hola, soy ${miNombre}! contáctame en ${email}`
+console.log(messageIterpolado)
+
+//Condicionales
